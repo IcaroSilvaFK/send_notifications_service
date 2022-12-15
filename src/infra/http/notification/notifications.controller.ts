1b99f78b505b5b40eae9ce2dbb5ae428e19aac67
whyy <icarovsilva1@gmail.com>
2022-12-15 19:04:16 -0300

    feat: create notification controller

--- src/infra/http/notification/notifications.controller.ts
+++ src/infra/http/notification/notifications.controller.ts
@@ -10,10 +10,55 @@ import {
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationDto } from './DTOs/create-notification.dto';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipients-notifications';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly sendNotification: SendNotification) {}
+  constructor(
+    private readonly sendNotification: SendNotification,
+    private readonly cancelNotification: CancelNotification,
+    private readonly readNotification: ReadNotification,
+    private readonly unreadNotification: UnreadNotification,
+    private readonly countRecipientNotification: CountRecipientNotification,
+    private readonly getRecipientNotification: GetRecipientNotifications,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({ notificationId: id });
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    return await this.countRecipientNotification.execute({
+      recipientId,
+    });
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getRecipientNotification.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({ notificationId: id });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({ notificationId: id });
+  }
 
   @Post()
   async create(@Body() data: CreateNotificationDto) {
--- src/infra/http/notification/notifications.module.ts
+++ src/infra/http/notification/notifications.module.ts
@@ -3,10 +3,22 @@ import { NotificationsController } from './notifications.controller';
 
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { SendNotification } from 'src/application/use-cases/send-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipients-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotification,
+    GetRecipientNotifications,
+  ],
 })
 export class NotificationsModule {}
